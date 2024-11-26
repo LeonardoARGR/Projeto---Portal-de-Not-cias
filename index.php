a0d99594a58b028945914dca12b8f8670f2b015a
LeonardoARGR <leoaragaogris@outlook.com>
2024-11-25 21:10:21 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Portal</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row menu-bar">
+                <div class="col-4">
+                    <a href="user.php">Usuário</a>
+                </div>
+                <div class="col-4 text-center">
+                    <div class="titulo-site">Teste</div>
+                </div>
+                <div class="col-4 text-end">
+                    <div>Pesquisar</div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid main-container">
+            
+        </div>
+    </body>
+</html>
